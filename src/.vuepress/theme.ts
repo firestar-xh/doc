0dd1008014ee63d8e519f6462564600bd2634612
hcy <hcy12312@gmail.com>
2025-01-19 18:46:49 +0800

    2

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -84,7 +84,8 @@ export default hopeTheme({
       components: [
         "PDF",
         "Share",
-        "ArtPlayer"
+        "ArtPlayer",
+        "VidStack"
       ],
       componentOptions: {
         artPlayer: {
